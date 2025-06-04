1332b7fae8d30109cf70e50230abb3c01c124286
hola0123 <Aksespremier@gmail.com>
2025-06-04 21:47:58 +0700

    Update sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9id27l3m16"
+    "revision": "0.7h5b7rje1g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
